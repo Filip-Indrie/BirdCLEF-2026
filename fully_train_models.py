82489bd9b0c6e715781c1c5fb6bb691a90dfcbde
Filip-Indrie <indrie.filip2306@gmail.com>
2026-06-17 01:36:39 +0300

    BUG FIXES in training loops.

--- fully_train_models.py
+++ fully_train_models.py
@@ -48,11 +48,11 @@ if __name__ == '__main__':
             net.backbone_grad(False)
 
             train_model(
-                device, net, True, False,
-                lr_fine_tune_head, weight_decay_train, positive_label_smoothing, threshold_train, True,
-                train_iter_train, val_iter_train, pos_weights_train,
-                num_epochs_fine_tune_head, num_epochs_fine_tune_head, save_weights=False,
-                save_folder="3 Stage Training Loop (Soundscapes HEAD Fine-Tune)"
+                device, net, True, True,
+                lr_fine_tune_head, weight_decay_fine_tune, positive_label_smoothing, threshold_fine_tune, False,
+                train_iter_fine_tune, val_iter_fine_tune, pos_weights_fine_tune,
+                num_epochs_fine_tune_head, num_epochs_fine_tune_head + 1, save_weights=False,
+                save_folder="3 Stage Training Loop (Soundscapes Backbone Fine-Tune)"
             )
 
             net.backbone_grad(True)
@@ -61,7 +61,7 @@ if __name__ == '__main__':
                 device, net, True, True,
                 lr_fine_tune_all, weight_decay_fine_tune, positive_label_smoothing, threshold_fine_tune, False,
                 train_iter_fine_tune, val_iter_fine_tune, pos_weights_fine_tune,
-                lr_fine_tune_all, patience_fine_tune, save_weights=True,
+                num_epochs_fine_tune_all, patience_fine_tune, save_weights=True,
                 save_folder="3 Stage Training Loop (Soundscapes Backbone Fine-Tune)"
             )
 
